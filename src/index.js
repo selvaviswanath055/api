52136f36cb5445015f426be14cbe38199a7d5805
vishwa <selvaviswanath055@gmail.com>
2022-03-06 16:18:54 +0530

    final - tested for production

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const db = require("./db");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
 db.connect(DB_HOST);
 
@@ -47,7 +47,11 @@ async function startApolloServer(typeDefs, resolvers) {
 
   server.applyMiddleware({ app, path: "/api" });
 
-  app.listen({ port }, () => console.log(`hi, im listening on ${port}`));
+  app.listen({ port }, () =>
+    console.log(
+      `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+    )
+  );
 }
 
 startApolloServer(typeDefs, resolvers);
